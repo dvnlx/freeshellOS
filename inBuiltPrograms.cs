742a61b347230d344455a47990cdb0a9fa67ed50
iDevYT <alphangred57@gmail.com>
2023-05-26 22:54:25 +0200

    made it more files and made color compatible with cosmosOS

---
+++ inBuiltPrograms.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace freeshellOS
+{
+	internal class inBuiltPrograms
+	{
+			private static Dictionary<string, ConsoleColor> colorDictionary;
+
+			public static void color(string? argcol)
+			{
+				if(!string.IsNullOrEmpty(argcol)) { 
+				InitializeColorDictionary();
+
+				if (colorDictionary.ContainsKey(argcol))
+				{
+					ConsoleColor consoleColor = colorDictionary[argcol];
+					Console.ForegroundColor = consoleColor;
+					Console.WriteLine($"Set to {argcol!}");
+				}
+				else
+				{
+					Console.WriteLine("Invalid color name. (FS02)");
+				}
+				}
+				else
+				{
+					Console.WriteLine("No color! (FS03)");
+				}
+			}
+
+			private static void InitializeColorDictionary()
+			{
+				colorDictionary = new Dictionary<string, ConsoleColor>()
+				{
+					{ "black", ConsoleColor.Black },
+					{ "blue", ConsoleColor.Blue },
+					{ "cyan", ConsoleColor.Cyan },
+					{ "gray", ConsoleColor.Gray },
+					{ "green", ConsoleColor.Green },
+					{ "magenta", ConsoleColor.Magenta },
+					{ "red", ConsoleColor.Red },
+					{ "yellow", ConsoleColor.Yellow }
+				};
+			}
+		public static void resetColor()
+		{
+			Console.ResetColor();
+			Console.WriteLine("Color Reset!");
+		}
+	}
+}
---
+++ shell.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Color = System.ConsoleColor;
+
+namespace freeshellOS
+{
+	internal class shell
+	{
+		public static void createInstance()
+		{
+			bool commands = true;
+			while (commands)
+			{
+				Console.Write("( ~/ )");
+				Console.Write("> ");
+				Console.Write("> ");
+				Console.Write("§ ");
+				string? input = Console.ReadLine();
+				if (!string.IsNullOrEmpty(input))
+				{
+
+					switch (input)
+					{
+						case "help":
+							Console.WriteLine("W.I.P");
+							break;
+						case "about":
+							Console.WriteLine("FreeshellOS v0.1");
+							break;
+						case "hello":
+							System.Console.WriteLine("Hello World!");
+							break;
+						case "shutdown":
+							Console.Clear();
+							System.Console.WriteLine("Goodbye!");
+							Thread.Sleep(1000);
+							Cosmos.System.Power.Shutdown();
+							break;
+						case "reboot":
+							Console.Clear();
+							Console.WriteLine("Rebooting...");
+							Thread.Sleep(1000);
+							Cosmos.System.Power.Shutdown();
+							break;
+						case var str when str.StartsWith("color -s"):
+							input = input.Replace("color -s ", "");
+							inBuiltPrograms.color(input);
+							break;
+						case "color -r":
+							inBuiltPrograms.resetColor();
+							break;
+						default:
+							Console.WriteLine($"{input} Not Found. Type help for help. (FS01)");
+							break;
+					}
+				}
+				else
+				{
+					System.Console.WriteLine("No command entered (FS05)");
+				}
+			}
+		}
+	}
+}
