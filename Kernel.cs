19961c9c6aaa755e46512264237d398cd880a22e
iDevYT <alphangred57@gmail.com>
2023-05-26 18:57:01 +0200

    init

---
+++ Kernel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Sys = Cosmos.System;
+
+namespace freeshellOS
+{
+	public class Kernel : Sys.Kernel
+	{
+
+		protected override void BeforeRun()
+		{
+			Console.WriteLine("FreeshellOS!");
+		}
+
+		protected override void Run()
+		{
+			Console.Write("Input: ");
+			var input = Console.ReadLine();
+			Console.Write("Text typed: ");
+			Console.WriteLine(input);
+		}
+	}
+}
